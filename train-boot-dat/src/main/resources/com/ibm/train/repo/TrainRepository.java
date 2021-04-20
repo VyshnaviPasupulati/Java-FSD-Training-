818bb55329a8b366c791464bf1d1e78f481c9a5c
VyshnaviPasupulati <pmunivyshnavi@gmail.com>
2021-04-20 10:25:28 +0530

    Spring Web Project

---
+++ train-boot-dat/src/main/resources/com/ibm/train/repo/TrainRepository.java
@@ -0,0 +1,18 @@
+package com.ibm.train.repo;
+
+import java.util.Collection;
+
+import com.ibm.train.pojo.Train;
+
+public interface TrainRepository {
+
+int saveTrain(Train t);
+	
+	Train fetchTrain(int tcode);
+	
+	Collection<Train> fetchAll();
+	
+	void delTrain(int tcode);
+}
+
+
