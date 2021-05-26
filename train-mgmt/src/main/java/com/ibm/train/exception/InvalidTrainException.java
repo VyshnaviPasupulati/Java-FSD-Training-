9edbca2a2aabd7dd992303b1871a9167116d9425
VyshnaviPasupulati <pmunivyshnavi@gmail.com>
2021-05-26 20:10:40 +0530

    Boot Train Management

---
+++ train-mgmt/src/main/java/com/ibm/train/exception/InvalidTrainException.java
@@ -0,0 +1,16 @@
+package com.ibm.train.exception;
+
+public class InvalidTrainException extends Exception{
+
+	public InvalidTrainException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTrainException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
